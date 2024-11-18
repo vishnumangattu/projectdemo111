a15f69cf7fd9ae8427037d42000ce12454da4f4d
vishnu <vishnumangattu03@gmail.com>
2024-11-18 10:30:43 +0530

    Update commit.py

--- commit.py
+++ commit.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 # Open log.txt to append a contribution entry
 with open("log.txt", "a") as file:
-    # 70% chance to skip contributions on some days
-    if random.random() < 0.98:
+    # 20% chance to skip contributions on some days
+    if random.random() < 0.2:
         file.write(f"No contribution on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         print("No contributions today!")
     else:
-        # Random number of contributions (between 1 and 5)
+        # Random number of contributions (between 3 and 9)
         num_contributions = random.randint(3, 9)
         for _ in range(num_contributions):
             file.write(f"Contribution on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
